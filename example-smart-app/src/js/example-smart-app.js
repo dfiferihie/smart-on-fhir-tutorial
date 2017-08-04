8708ea3202e74b5e787bd5a0760970e776f8f4ca
SMARTChestpain <30666710+SMARTChestpain@users.noreply.github.com>
2017-08-04 15:48:09 -0400

    title change

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -1,39 +1,46 @@
+
 (function(window){
   window.extractData = function() {
     var ret = $.Deferred();
 
-    function onError() {
+    function onError() { // generic error catching
       console.log('Loading error', arguments);
       ret.reject();
     }
 
+
     function onReady(smart)  {
-      if (smart.hasOwnProperty('patient')) {
-        var patient = smart.patient;
+      if (smart.hasOwnProperty('patient')) {//if a patient is being handed to the program, do all of the following...
+
+        var patient = smart.patient; //stores patient context
         var pt = patient.read();
-        var obv = smart.patient.api.fetchAll({
-                    type: 'Observation',
+        var obv = smart.patient.api.fetchAll({//where the patient API's/FHIR resources can be fetched and stored
+                    type: 'Observation',//FHIR resource Observation
                     query: {
-                      code: {
-                        $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
-                              'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
-                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
+                      code: {//LOIC codes
+                        $or: ['http://loinc.org|8302-2', //for height
+                              'http://loinc.org|8462-4', //for diastolic bp
+                              'http://loinc.org|8480-6', //for systolic bp
+                              'http://loinc.org|2085-9', //for hdl
+                              'http://loinc.org|2089-1', //for ldl
+                              'http://loinc.org|55284-4', //generic bp code
+                              'http://loinc.org|18810-2']//(Akeem's experiment)last code for EKG
                       }
                     }
                   });
 
-        $.when(pt, obv).fail(onError);
+        $.when(pt, obv).fail(onError); //for error catching
 
-        $.when(pt, obv).done(function(patient, obv) {
-          var byCodes = smart.byCodes(obv, 'code');
-          var gender = patient.gender;
-          var dob = new Date(patient.birthDate);
-          var day = dob.getDate();
-          var monthIndex = dob.getMonth() + 1;
-          var year = dob.getFullYear();
+        $.when(pt, obv).done(function(patient, obv) { //when patient/his or her tests have been fetched
+          var byCodes = smart.byCodes(obv, 'code'); 
+          var gender = patient.gender; //patient gender
+          var dob = new Date(patient.birthDate); //patient DOB
+          var day = dob.getDate(); //gets date from DOB
+          var monthIndex = dob.getMonth() + 1; //gets month
+          var year = dob.getFullYear(); //gets year
 
-          var dobStr = monthIndex + '/' + day + '/' + year;
-          var fname = '';
+          var dobStr = monthIndex + '/' + day + '/' + year; //formatting for DOB
+          var fname = ''; //variable holders for first and last name of patient
           var lname = '';
 
           if (typeof patient.name[0] !== 'undefined') {
@@ -41,35 +48,57 @@
             lname = patient.name[0].family.join(' ');
           }
 
-          var height = byCodes('8302-2');
-          var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
+          var height = byCodes('8302-2'); //getting height using method "byCodes", with LOINC code being used as input 
+          var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');//getting bps...
           var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
-          var hdl = byCodes('2085-9');
-          var ldl = byCodes('2089-1');
+          var hdl = byCodes('2085-9');//getting hdl
+          var ldl = byCodes('2089-1');//getting ldl
 
-          var p = defaultPatient();
-          p.birthdate = dobStr;
-          p.gender = gender;
-          p.fname = fname;
-          p.lname = lname;
-          p.age = parseInt(calculateAge(dob));
-          p.height = getQuantityValueAndUnit(height[0]);
+          var EKG = byCodes('18810-2'); //(experimenting) EKG value
 
-          if (typeof systolicbp != 'undefined')  {
+          var systolicbpDate = byCodes('55284-4').effectiveDateTime; // (experimenting) trying to access date of systolicbp test
+
+          var p = defaultPatient(); //variable that will be used to hold all patient information extracted
+
+
+
+          p.birthdate = dobStr; //patient p's DOB string being placed instance variable birthdate
+          p.gender = gender; //p's gender being put in variable gender
+          p.fname = fname; //fname being placed in p's instance variable fname
+          p.lname = lname;//lname being placed in p's instance variable lname
+          p.age = parseInt(calculateAge(dob)); //the same applies to the age...
+          p.height = getQuantityValueAndUnit(height[0]);//the same applies to the height
+
+          p.EKG = EKG;//(experimenting) EKG VALUE
+          if (typeof EKG != 'undefined'){ //if EKG exists, put in patient's EKG variable
+            p.EKG = EKG;
+          }
+          else
+            p.EKG = "EKG doesn't exist"; //else, save as nonexistent
+
+
+          if (typeof systolicbpDate != 'undefined'){ //if systolic date exists
+            p.systolicbpDate = 'systolicbpDate exists'; //save as "exists"
+          }
+          else
+            p.systolicbpDate = 'doesn"t exist'; //"save as DNE"
+
+
+          if (typeof systolicbp != 'undefined')  { //if systolic exists, save value in p's systolicbp variable
             p.systolicbp = systolicbp;
           }
 
-          if (typeof diastolicbp != 'undefined') {
+          if (typeof diastolicbp != 'undefined') { //if diastolic exists, save value in p's diastolicbp variable
             p.diastolicbp = diastolicbp;
           }
 
-          p.hdl = getQuantityValueAndUnit(hdl[0]);
-          p.ldl = getQuantityValueAndUnit(ldl[0]);
+          p.hdl = getQuantityValueAndUnit(hdl[0]); //get hdl value and unit
+          p.ldl = getQuantityValueAndUnit(ldl[0]); //get ldl value and unit
 
           ret.resolve(p);
         });
-      } else {
-        onError();
+      } else {//else if no patient is being handed to the program, run onError method...
+        onError(); 
       }
     }
 
@@ -78,7 +107,7 @@
 
   };
 
-  function defaultPatient(){
+  function defaultPatient(){ //holds the "instance variables" of p
     return {
       fname: {value: ''},
       lname: {value: ''},
@@ -90,10 +119,13 @@
       diastolicbp: {value: ''},
       ldl: {value: ''},
       hdl: {value: ''},
+
+      EKG: {value: ''},//EKG (test)
+      systolicbpDate: {value: ''},//systolic bp date (test)
     };
   }
 
-  function getBloodPressureValue(BPObservations, typeOfPressure) {
+  function getBloodPressureValue(BPObservations, typeOfPressure) { //function that gets bp from inputs
     var formattedBPObservations = [];
     BPObservations.forEach(function(observation){
       var BP = observation.component.find(function(component){
@@ -110,11 +142,11 @@
     return getQuantityValueAndUnit(formattedBPObservations[0]);
   }
 
-  function isLeapYear(year) {
+  function isLeapYear(year) { //function that tells if it's a leapyear
     return new Date(year, 1, 29).getMonth() === 1;
   }
 
-  function calculateAge(date) {
+  function calculateAge(date) { //function that calculates age of patient given DOB
     if (Object.prototype.toString.call(date) === '[object Date]' && !isNaN(date.getTime())) {
       var d = new Date(date), now = new Date();
       var years = now.getFullYear() - d.getFullYear();
@@ -131,7 +163,7 @@
     }
   }
 
-  function getQuantityValueAndUnit(ob) {
+  function getQuantityValueAndUnit(ob) { //gives valueQuantity of a test
     if (typeof ob != 'undefined' &&
         typeof ob.valueQuantity != 'undefined' &&
         typeof ob.valueQuantity.value != 'undefined' &&
@@ -142,7 +174,10 @@
     }
   }
 
-  window.drawVisualization = function(p) {
+  window.drawVisualization = function(p) {  
+  //draws out the final visualization on the
+  //webpage UI... substitutes HTML tags (#____) for p's instance 
+  //variables stored in patient context "p"
     $('#holder').show();
     $('#loading').hide();
     $('#fname').html(p.fname);
@@ -151,10 +186,13 @@
     $('#birthdate').html(p.birthdate);
     $('#age').html(p.age);
     $('#height').html(p.height);
-    $('#systolicbp').html(p.systolicbp);
+    //$('#systolicbp').html(p.systolicbp);
     $('#diastolicbp').html(p.diastolicbp);
     $('#ldl').html(p.ldl);
     $('#hdl').html(p.hdl);
+
+    $('#EKG').html(p.EKG);//EKG
+
   };
 
 })(window);
